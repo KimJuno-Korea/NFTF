f0074a2ccb1f9b78311edb6a37a7756142c42307
jhpark010 <ababcd8308@naver.com>
2021-05-04 00:45:44 +0900

    add BoardController and ReplyController

--- nftf/src/main/java/kr/co/nftf/reply/ReplyController.java
+++ nftf/src/main/java/kr/co/nftf/reply/ReplyController.java
@@ -1,10 +1,32 @@
 package kr.co.nftf.reply;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
+/* @RequestMapping("/reply") */
 public class ReplyController {
-
+	@PostMapping("/reply/{no}")
+	public ModelAndView registReply(Reply reply) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/board/{boardNo}"); //redirect할 때 게시글번호 담아야함
+		
+		return modelAndView;
+	}
+	
+	@PutMapping("/reply/{no}")
+	public ModelAndView editReply(Reply reply) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/board/{boardNo}"); //redirect할 때 게시글번호 담아야함
+		
+		return modelAndView;
+	}
+	
+	@DeleteMapping("/reply/{no}")
+	public ModelAndView deleteReply(Reply reply) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/board/{boardNo}"); //redirect할 때 게시글번호 담아야함
+		
+		return modelAndView;
+	}
 }
