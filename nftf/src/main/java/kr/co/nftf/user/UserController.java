390941b8158449e5894f2a56c64708d355cc51ad
DOKKU2714 <forkboy159@gmail.com>
2021-05-04 14:26:58 +0900

    CommonController.java 및 UserController.java 껍질 구현 완료

--- nftf/src/main/java/kr/co/nftf/user/UserController.java
+++ nftf/src/main/java/kr/co/nftf/user/UserController.java
@@ -1,8 +1,10 @@
 package kr.co.nftf.user;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
@@ -16,8 +18,9 @@ public class UserController {
 	
 	@PostMapping("/user")
 	public ModelAndView signup(User user) {
+		ModelAndView modelAndView = new ModelAndView(new RedirectView("/login"));
 		//todo
-		return new ModelAndView(new RedirectView("/common/login"));
+		return modelAndView;
 	}
 	
 	@GetMapping("/id/form")
@@ -38,6 +41,70 @@ public class UserController {
 	
 	@PostMapping("/password")
 	public ModelAndView findPassword(User user) {
-		return new ModelAndView("/user/password/edit");
+		ModelAndView modelAndView = new ModelAndView("/user/password/edit");
+		//todo
+		return modelAndView;
+	}
+	
+	@PostMapping("/password/{id}")
+	public ModelAndView editPassword(User user) {
+		ModelAndView modelAndView = new ModelAndView(new RedirectView("/login"));
+		//todo
+		return modelAndView;
+	}
+	
+	@GetMapping("/user/form/{id}")
+	public ModelAndView myPageForm(User user) {
+		ModelAndView modelAndView = new ModelAndView("/user/mypage/form");
+		//todo
+		return modelAndView;
+	}
+	
+	@PostMapping("/user/form/{id}")
+	public ModelAndView myPageLogin(User user) {
+		ModelAndView modelAndView = new ModelAndView("/user/mypage/index");
+		//todo
+		return modelAndView;
+	}
+	
+	@GetMapping("/user/{id}/form")
+	public ModelAndView editUserForm(User user) {
+		ModelAndView modelAndView = new ModelAndView("/user/mypage/edit");
+		//todo
+		return modelAndView;
+	}
+	
+	@PutMapping("/user/{id}")
+	public ModelAndView editUser(User user) {
+		ModelAndView modelAndView = new ModelAndView(new RedirectView("/logout"));
+		//todo
+		return modelAndView;
+	}
+	
+	@GetMapping("/{id}")
+	public ModelAndView createLoginQR(User user) {
+		ModelAndView modelAndView = new ModelAndView("/user/mypage/qr");
+		//todo
+		return modelAndView;
+	}
+	
+	@GetMapping("/user/trading/{id}")
+	public ModelAndView getTradingList (User user){
+		ModelAndView modelAndView = new ModelAndView("/user/mypage/trading");
+		//todo
+		return modelAndView;
+	}
+	
+	@GetMapping("/user/withdrawal/{id}")
+	public ModelAndView withdrawalForm(User user) {
+		//todo
+		ModelAndView modelAndView = new ModelAndView("/user/mypage/withdrawal");
+		return modelAndView;
+	}
+	
+	@DeleteMapping("/user/{id}")
+	public ModelAndView withdrawal(User user) {
+		ModelAndView modelAndView = new ModelAndView(new RedirectView("/logout"));
+		return modelAndView;
 	}
 }
