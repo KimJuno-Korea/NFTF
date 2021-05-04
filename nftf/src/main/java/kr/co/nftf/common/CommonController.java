cb964659637914492969cbd1120fc5d00e19e075
DOKKU2714 <forkboy159@gmail.com>
2021-05-04 13:34:53 +0900

    - common, user jsp 폴더 추가 및 jsp 모두 추가 - commonController, userController 수정

--- nftf/src/main/java/kr/co/nftf/common/CommonController.java
+++ nftf/src/main/java/kr/co/nftf/common/CommonController.java
@@ -1,39 +1,37 @@
 package kr.co.nftf.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import kr.co.nftf.user.User;
 import kr.co.nftf.user.UserServiceImpl;
 
-@Controller
-@RequestMapping("/common")
+@RestController
 public class CommonController {
 	@Autowired
 	private UserServiceImpl userService;
 	
-	@GetMapping("/index")
+	@GetMapping("/common/index")
 	public ModelAndView index() {
 		return new ModelAndView("/common/index");
 	}
 	
-	@GetMapping("/login")
+	@GetMapping("/common/login")
 	public ModelAndView loginForm() {
 		return new ModelAndView("/common/login");
 	}
 	
-	@PostMapping("/login")
+	@PostMapping("/common/login")
 	public ModelAndView login(User user) {
 		
 		return new ModelAndView(new RedirectView("/nftf/common/index"));
 	}
 	
-	@GetMapping("/logout")
+	@GetMapping("/common/logout")
 	public ModelAndView logout() {
 		
 		return new ModelAndView(new RedirectView("/nftf/common/index"));
--- nftf/src/main/java/kr/co/nftf/user/UserController.java
+++ nftf/src/main/java/kr/co/nftf/user/UserController.java
@@ -1,9 +1,43 @@
 package kr.co.nftf.user;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
-@RequestMapping("/")
+@RestController
 public class UserController {
+	@GetMapping("/user/form")
+	public ModelAndView signupForm() {
+		return new ModelAndView("/user/signup");
+	}
+	
+	@PostMapping("/user")
+	public ModelAndView signup(User user) {
+		//todo
+		return new ModelAndView(new RedirectView("/common/login"));
+	}
+	
+	@GetMapping("/id/form")
+	public ModelAndView findIdForm() {
+		return new ModelAndView("/user/id/find");
+	}
+	
+	@PostMapping(value="/id", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public User findId(User user) {
+		//
+		return user;
+	}
+	
+	@GetMapping("/password/form")
+	public ModelAndView findPasswordForm() {
+		return new ModelAndView("/user/password/find");
+	}
+	
+	@PostMapping("/password")
+	public ModelAndView findPassword(User user) {
+		return new ModelAndView("/user/password/edit");
+	}
 }
