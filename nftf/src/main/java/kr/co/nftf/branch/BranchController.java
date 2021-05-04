0dab9b12babb2d0429f5b504d174343d25dc0844
KimJuno-Korea <kimjuno.korea@gmail.com>
2021-05-04 13:31:54 +0900

    TradingBoxController, BranchController modified by KIMJUNO-KOREA

--- nftf/src/main/java/kr/co/nftf/branch/BranchController.java
+++ nftf/src/main/java/kr/co/nftf/branch/BranchController.java
@@ -1,16 +1,16 @@
 package kr.co.nftf.branch;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class BranchController {
 	@Autowired
