ea4c5f137031ae1697b6421c4902a825c266b266
dohyun kim <forkboy159@gmail.com>
2021-05-24 22:03:44 +0900

    스프링 부트 설정 변경

--- nftf/src/main/java/kr/co/nftf/NftfApplication.java
+++ nftf/src/main/java/kr/co/nftf/NftfApplication.java
@@ -1,12 +1,8 @@
 package kr.co.nftf;
 
-import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -17,18 +13,4 @@ public class NftfApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NftfApplication.class, args);
 	}
-	
-	@Bean
-    public ServletWebServerFactory serverFactory() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(80);
-        return connector;
-        
-    }
 }
