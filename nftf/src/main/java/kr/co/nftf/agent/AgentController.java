05057024a4cbbe59c439210afc2c0c6839a64331
KIMJUN5 <dythaos@gmail.com>
2021-06-06 23:25:30 +0900

    [AgentController] 물품 수령, 인증키 대조 추가 및 물품 등록 메소드 수정

--- nftf/src/main/java/kr/co/nftf/agent/AgentController.java
+++ nftf/src/main/java/kr/co/nftf/agent/AgentController.java
@@ -1,5 +1,6 @@
 package kr.co.nftf.agent;
 
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import kr.co.nftf.board.Board;
 import kr.co.nftf.board.BoardService;
 import kr.co.nftf.branch.Branch;
+import kr.co.nftf.security.SecurityService;
+import kr.co.nftf.trading.Trading;
+import kr.co.nftf.trading.TradingService;
 import kr.co.nftf.tradingbox.TradingBox;
 import kr.co.nftf.tradingbox.TradingBoxService;
 import kr.co.nftf.user.User;
@@ -29,6 +33,12 @@ public class AgentController {
 	
 	@Autowired
 	private BoardService boardServiceImpl;
+	
+	@Autowired
+	private TradingService tradingServiceImpl;
+	
+	@Autowired
+	private SecurityService securityServiceImpl;
 	
 	// 회원 검증
 	@PostMapping(value="/agent/authentication", consumes=MediaType.APPLICATION_JSON_VALUE)
@@ -47,6 +57,21 @@ public class AgentController {
 		return authResult;
 	}
 	
+	// 인증키 대조
+	@PostMapping(value="/agent/contrastkey", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Boolean> contrastKey (@RequestBody String authKey) throws Exception {
+		Map<String, Boolean> authResult = new HashMap<String, Boolean>();
+		
+		if (authKey != null) {
+				authResult.put("result", securityServiceImpl.contrastKey(authKey));
+			
+				return authResult;
+			}
+		authResult.put("result", false);
+		
+		return authResult;
+	}
+	
 	// 거래함 목록 조회
 	@GetMapping(value = "/agent/{code}/tradingbox")
 	public List<TradingBox> getTradingBoxListToJson(Branch branch) throws Exception {
@@ -62,12 +87,47 @@ public class AgentController {
 	}
 	
 	// 물품 등록
-	@PostMapping(value="/agent/registGoods", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value="/agent/registgoods", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Map<String, Boolean> registGoods (@RequestBody TradingBox tradingBox) {
 		Map<String, Boolean> result = new HashMap<String, Boolean>();	
-		try {
+		Board board = new Board();
+		Trading trading = new Trading();
+		try {	
+				tradingBox.setStatus('T');
+				board.setNo(tradingBox.getBoardNo());
+				board.setTradeWay('W');
+				board.setStatus('W');
+				trading.setBoardNo(tradingBox.getBoardNo());
+				trading.setStatus('T');
+				
 				tradingBoxServiceImpl.editTradingBox(tradingBox);
+				boardServiceImpl.boardEdit(board);
+				tradingServiceImpl.editTrading(trading);
+				//tradingBoxServiceImpl
 				result.put("result", true);
+		} catch (Exception e) {
+				result.put("result", false);
+		}
+		return result;
+	}
+	
+	// 물품 수령
+	@PostMapping(value="/agent/receivegoods", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Boolean> receiveGoods (@RequestBody TradingBox tradingBox) {
+		Map<String, Boolean> result = new HashMap<String, Boolean>();
+		Board board = new Board();
+		Trading trading = new Trading();
+		try {
+			tradingBox.setStatus('F');
+			board.setNo(tradingBox.getBoardNo());
+			board.setStatus('S');
+			trading.setStatus('S');
+			trading.setBoardNo(tradingBox.getBoardNo());
+			
+			tradingBoxServiceImpl.editTradingBox(tradingBox);
+			boardServiceImpl.boardEdit(board);
+			tradingServiceImpl.editTrading(trading);
+			result.put("result", true);
 		} catch (Exception e) {
 				result.put("result", false);
 		}
